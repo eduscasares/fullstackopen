774a1a08c78476449a019fa1d36981f4c91943b9
eduscasares <eduardo.scasares@gmail.com>
2023-08-01 16:41:54 +0200

    Phone guide exercise

--- part2/phone-guide/src/App.js
+++ part2/phone-guide/src/App.js
@@ -2,39 +2,49 @@ import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonsForm from './components/PersonsForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
-  const [filteredPersons, setFilteredPersons] = useState(persons); 
+  const [filteredPersons, setFilteredPersons] = useState(persons);   
+
 
   useEffect(() => {
-    // Actualizar filteredPersons cada vez que persons cambia
-    setFilteredPersons(
-      persons.filter((person) =>
-        person.name.toLowerCase().includes(searchQuery.toLowerCase())
+  
+      axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          setPersons(response.data)
+        })
+
+      setFilteredPersons(
+        persons.filter((person) =>
+          person.name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
       )
-    );
-  }, [persons, searchQuery]);
+
+  }, [persons, searchQuery])
+
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     let aux = persons.map((element) => element.name);
-    let newPerson;
 
     if (aux.includes(newName)) {
       alert(`${newName} is already added to your phonebook`);
     } else {
-      newPerson = [...persons, { name: newName, number: newNumber }];
-      setPersons(newPerson);
+
+      axios
+        .post('http://localhost:3001/persons', {
+          name: newName,
+          number: newNumber
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+
       setNewName('')
       setNewNumber('')
     }
