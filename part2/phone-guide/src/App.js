9060ebf186d5264d0f7b04d57724af5e472b4c43
eduscasares <eduardo.scasares@gmail.com>
2023-09-11 14:27:05 +0200

    Phone guid exercises completion

--- part2/phone-guide/src/App.js
+++ part2/phone-guide/src/App.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonsForm from './components/PersonsForm';
 import Persons from './components/Persons';
+import Notification from './components/Notification';
 import axios from 'axios';
+import './index.css'
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -10,32 +12,24 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredPersons, setFilteredPersons] = useState(persons);   
+  const [errorMessage, setErrorMessage] = useState(null)
 
+  const getAllPersons = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
 
   useEffect(() => {
-  
-      axios
-        .get('http://localhost:3001/persons')
-        .then(response => {
-          setPersons(response.data)
-        })
-
-      setFilteredPersons(
-        persons.filter((person) =>
-          person.name.toLowerCase().includes(searchQuery.toLowerCase())
-        )
-      )
-
-  }, [persons, searchQuery])
-
+    getAllPersons();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     let aux = persons.map((element) => element.name);
-    // let auxNumber = persons.map((element) => element.number);
-    // let auxId = persons.map((element) => element.id);
-    console.log(aux)
-
 
     if (aux.includes(newName)) {
       if (window.confirm(`${newName} is already added to your phonebook, replace the old number with a new one?`)) {
@@ -44,7 +38,18 @@ const App = () => {
             name: newName,
             number: newNumber
           })
-          .catch(error => console.log(error))
+          .then(() => {
+            getAllPersons();
+          })
+          .catch(error => {
+            setErrorMessage(
+              `Cannot add ${newName} to phonebook`
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+    
+          })
       }
     } else {
 
@@ -52,6 +57,10 @@ const App = () => {
         .post('http://localhost:3001/persons', {
           name: newName,
           number: newNumber
+        })
+        .then(() => {
+          // setNotice(`${newName} updated`)
+          getAllPersons();
         })
         .catch(error => console.log(error))
 
@@ -82,6 +91,7 @@ const App = () => {
     if(window.confirm(`Delete ${persons.find((person) => person.id === id).name}?`)) {
       axios
         .delete(`http://localhost:3001/persons/${id}`)
+        .then(() => getAllPersons())
         .catch(error => console.log(error))
     }
   }
@@ -89,6 +99,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={errorMessage} />
      
       <Filter 
         searchQuery={searchQuery} 
@@ -108,7 +120,7 @@ const App = () => {
       <h2>Numbers</h2>
       
       <Persons 
-        filteredPersons={filteredPersons} 
+        filteredPersons={searchQuery === '' ? persons : filteredPersons} 
         handleDelete={handleDelete}
       />
 
